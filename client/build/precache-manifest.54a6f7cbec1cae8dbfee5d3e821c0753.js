56a760f4b1f2ffdf3a6b8195ad47279b999da881
mehboobwaseem16 <mehboobwaseem16@gmail.com>
2020-05-26 19:24:33 +0000

    Waseem-CA4

---
+++ client/build/precache-manifest.54a6f7cbec1cae8dbfee5d3e821c0753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0eec6cc54008d54cce2cb263aaa2de3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba363911852655f5d24",
+    "url": "/static/css/2.5578688d.chunk.css"
+  },
+  {
+    "revision": "5a5d64fc1a1f56f483cc",
+    "url": "/static/css/main.ea1027fc.chunk.css"
+  },
+  {
+    "revision": "eba363911852655f5d24",
+    "url": "/static/js/2.920adab3.chunk.js"
+  },
+  {
+    "revision": "5f7a28fc5451493881f8698ab05bc8b8",
+    "url": "/static/js/2.920adab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5d64fc1a1f56f483cc",
+    "url": "/static/js/main.8e0b0422.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
